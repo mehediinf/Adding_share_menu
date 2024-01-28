5d9e7b82fc6e7ab16aafa3d9693e82c74dcac5b6
Md Mehedi Hasan <145797601+mehediinf@users.noreply.github.com>
2024-01-28 06:48:36 +0600

    Second Commit Adding_share_menu

--- app/src/main/java/com/example/adding_share_menu/MainActivity.java
+++ app/src/main/java/com/example/adding_share_menu/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.adding_share_menu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_layout,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()==R.id.shared_Id){
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+
+            String subject = "Multimedia app";
+            String body = "This is very useful App.\n com.example.adding_share_menu";
+
+            intent.putExtra(Intent.EXTRA_SUBJECT,subject);
+            intent.putExtra(Intent.EXTRA_TEXT,body);
+
+            startActivity(Intent.createChooser(intent,"share with"));
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
